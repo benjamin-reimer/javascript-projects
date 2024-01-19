abfd3f4aac42ad487c5b3c9d18a86b264e0dd1a1
Benjamin Reimer <benjamin@Benjamins-MBP.attlocal.net>
2024-01-18 20:49:49 -0600

    class three studio

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -1,14 +1,27 @@
+const input = require('readline-sync');
+
 let food = "water bottles,meal packs,snacks,chocolate";
 let equipment = "space suits,jet packs,tool belts,thermal detonators";
 let pets = "parrots,cats,moose,alien eggs";
 let sleepAids = "blankets,pillows,eyepatches,alarm clocks";
 
 //1) Use split to convert the strings into four cabinet arrays. Alphabetize the contents of each cabinet.
-
+let cab0 = food.split(',').sort();
+let cab1 = equipment.split(',').sort();
+let cab2 = pets.split(',').sort();
+let cab3 = sleepAids.split(',').sort();
 //2) Initialize a cargoHold array and add the cabinet arrays to it. Print cargoHold to verify its structure.
+let cargoHold = [];
+
+cargoHold.push(cab0,cab1,cab2,cab3);
+console.log(cargoHold);
 
 //3) Query the user to select a cabinet (0 - 3) in the cargoHold.
-
+let cabSelection = input.question('Please select a cabinet. (0-3) ');
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
-
+    if (cabSelection <= 3 && cabSelection > 0) {
+    console.log(`Cabinet number ${cabSelection} contains ${cargoHold[cabSelection]}`)
+    } else {
+    console.log('ERROR INVALID SELECTION.')
+}
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -1,11 +1,25 @@
 const input = require('readline-sync');
 let str = "LaunchCode";
 
+
+let numLetters = input.question('How many letters would you like to relocate? ');
+
+if (numLetters > str.length || numLetters <= 0) {
+    numLetters = 3;
+}
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
+let slicedString = str.slice(0,numLetters);
+let secondSlice =  str.slice(numLetters,10);
+let finalString = secondSlice + slicedString;
+
+
+//console.log(`The word 'LaunchCode' becomes '${finalString} in speudo Pig Latin.`);
 
 //Use a template literal to print the original and modified string in a descriptive phrase.
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
+
+console.log(finalString);
 
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
